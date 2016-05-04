025aadb920c4ccf81646d60508f02c63540dc37b
Zagrebantsev Artiom <aptos@mail.ru>
2016-05-05 00:39:55 +0300

    First commit of test project.

---
+++ test/src/by/grodno/java/zagart/test/Main.java
@@ -0,0 +1,14 @@
+package by.grodno.java.zagart.test;
+
+/**
+ * Created by Программирование on 05.05.2016.
+ */
+public class Main {
+
+    public static void main(String []args) {
+
+        System.out.println("Hello World!");
+
+    }
+
+}
